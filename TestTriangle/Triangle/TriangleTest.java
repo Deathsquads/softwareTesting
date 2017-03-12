73ee2feed0623e489508fbbbb9b52780ee8f8431
Ge fanghai <tjuscs_Gefanghai@163.com>
2017-03-12 18:42:51 +0800

    1.0.1

--- TestTriangle/Triangle/TriangleTest.java
+++ TestTriangle/Triangle/TriangleTest.java
@@ -9,14 +9,17 @@ import static org.junit.Assert.*;
  * Created by admin on 2017/3/12.
  */
 public class TriangleTest {
-
+    private Triangle triangle;
     @Before
     public void setUp() throws Exception {
-
+        triangle = new Triangle();
     }
 
     @Test
     public void testT() throws Exception {
-
+        assertEquals("直角三角形",triangle.T(3,4,5));
+        assertEquals("等边三角形",triangle.T(3,3,3));
+        assertEquals("等腰三角形",triangle.T(3,3,4));
+        assertEquals("无法构成三角形",triangle.T(3,2,10));
     }
 }
--- src/Triangle/Triangle.java
+++ src/Triangle/Triangle.java
@@ -1,20 +1,28 @@
 package Triangle;
-
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
 /*
  * Created by admin on 2017/3/10.
  */
 public class Triangle {
-    public boolean IsAEquilateral(int a,int b,int c)
-    {
-        boolean IsAEquilateral = true;
-        if((a+b>c)&&(b+c>a)&&(a+c>b)&&(a==b||a==c||b==c)){
-            return IsAEquilateral;
+    public String T(double a,double b,double c){
+        double tem = Math.max(a, b);
+        if(tem>c){
+            if(tem==a){
+                a = c;
+            }else {
+                b = c;
+            }
+            c = tem;
         }
-        else{
-            IsAEquilateral = false;
-            return IsAEquilateral;
-        }
+        if(!(a+b>c&&Math.abs(a-b)<c)){
+            return "无法构成三角形";
+        }else if(a==b||a==c||b==c){
+            if (a==b && b==c)
+                return "等边三角形";
+            else return "等腰三角形";
+        }else if(a*a+b*b==c*c){
+            return "直角三角形";
+        }else if(a*a+b*b<c*c){
+            return "锐角三角形";
+        }else return "钝角三角形";
     }
 }
