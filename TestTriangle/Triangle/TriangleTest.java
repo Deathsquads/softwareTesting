6102a41d313b60b82a76e825731307286b6a6d82
Ge fanghai <tjuscs_Gefanghai@163.com>
2017-03-12 18:37:55 +0800

    1.0.1

---
+++ TestTriangle/Triangle/TriangleTest.java
@@ -0,0 +1,22 @@
+package Triangle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 2017/3/12.
+ */
+public class TriangleTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testT() throws Exception {
+
+    }
+}
